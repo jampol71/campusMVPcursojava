4a5ec5f6eeb62bb7346df2bcc5e702501323ece1
jampol <rafaellucenamartinez@mbp-de-rafael.home>
2017-03-16 09:20:00 +0100

    Creación programa CobrosPagos

---
+++ CobrosPagos/src/cobrospagos/CobrosPagos.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cobrospagos;
+
+
+import java.util.Date;
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+/**
+ *
+ * @author rafaellucenamartinez
+ */
+public class CobrosPagos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
